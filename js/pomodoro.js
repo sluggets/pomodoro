8e15bdab8c1f3995e44c85f7c2282bacbc8fe3a7
timothy <tim.longoria@gmail.com>
2016-08-21 09:52:34 -0500

    Deletes function, adds break timer
    Deleters old timer function, adds functionality for work timer to
    immediately flow into play timer

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
   var listen = document.getElementById("myButton");
   var initVal = document.getElementById("timer"); 
+  play = false;
   initVal.innerHTML = '1:00';
-  minutesToGo = 1;
-  secondsToGo = 60;
+  defaultPlay = document.getElementById("play").innerHTML;
+  defaultWork = document.getElementById("work").innerHTML;
+  minutesToGo = parseInt(defaultWork);
+  secondsToGo = parseInt(defaultWork) * 60;
   listen.addEventListener("click", function () {
      interValID = window.setInterval(displayTime, 1000); 
   });
@@ -13,7 +16,14 @@ function displayTime()
 {
   if (minutesToGo === 0 && secondsToGo === 0)
   {
+    if (play)
+    {
+      clearInterval(interValID);
+    }
     activateAlarm();
+    play = true;
+    minutesToGo = parseInt(defaultPlay);
+    secondsToGo = parseInt(defaultPlay) * 60;
   }
   var testElem = document.getElementById("timer");
   var secToPrint;
@@ -31,94 +41,18 @@ function displayTime()
 
 function activateAlarm()
 {
-  clearInterval(interValID);
+  //clearInterval(interValID);
+  //activateBreak();
   console.log("inside activateAlarm()");
   //TODO
 }
-/*function timerFunc()
+
+function incrementDecrement()
 {
-  var newSeconds;
-  var oldSeconds;
-  var newMinutes;
-  var oldMinutes;
-  var minToPrint;
-  var secToPrint;
-  var h1Timer = document.getElementById("timer");
-  while (secondsToGo > 0)
-  {
-    var curDate = new Date();
- 
-    newSeconds = curDate.getSeconds();
-    newMinutes = curDate.getMinutes();
-    if (oldSeconds != newSeconds)
-    {
-      if (minutesToGo < 10)
-      {
-        minToPrint = '0' + minutesToGo;
-      }
-      else
-      {
-        minToPrint =  minutesToGo;
-      }
 
-      if (secondsToGo < 10)
-      {
-        secToPrint = '0' + secondsToGo;
-      }
-      else
-      {
-        secToPrint =  secondsToGo;
-      }
+}
 
-      console.log(minToPrint + ':' + secToPrint);
-      h1Timer.innerHTML = minToPrint + ':' + secToPrint;
-
-      if (secondsToGo == 0)
-      {
-        minutesToGo--;
-        secondsToGo = 60;
-      }
-
-
-      secondsToGo--;
-    
-      oldSeconds = newSeconds;
-    }
-
-    if (oldMinutes != newMinutes)
-    {
-      oldMinutes = newMinutes; 
-    }
-  }
-
-  while (secondsToGo > 0)
-  {
-    var curDate = new Date();
-    minToPrint = '00';
-    newSeconds = curDate.getSeconds();
-    
-    if (oldSeconds != newSeconds)
-    {
-
-      if (secondsToGo < 10)
-      {
-        secToPrint = '0' + secondsToGo;
-      }
-      else
-      {
-        secToPrint = secondsToGo;
-      }
-
-      console.log(minToPrint + ':' + secToPrint);
-      h1Timer.innerHTML = minToPrint + ':' + secToPrint;
-
-      if (secondsToGo == 0)
-      {
-        secondsToGo = 60;
-      }
-
-      secondsToGo--;
-    
-      oldSeconds = newSeconds;
-    }
-  }*/
+function activateBreak()
+{
+  //TODO
+}
