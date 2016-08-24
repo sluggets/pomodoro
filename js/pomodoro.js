64262f9809bb1528661104a7527df019b59ae8cf
timothy <tim.longoria@gmail.com>
2016-08-24 11:26:36 -0500

    Adds comments, increment/decrement functions
    Adds explanatory comments,  four event listeners for clicks on the
    four increment/decrement buttons. Adds closure function that changes
    work and play timer values to be used in callback function inside
    a click listener, adds function that changes digits css for changes
    in width(double digit number vs single digit number)

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,14 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
+  
   var listen = document.getElementById("myButton");
-  var initVal = document.getElementById("timer"); 
+  //var initVal = document.getElementById("timer"); 
+  var playInc = document.getElementById("play-inc");
+  var playDec = document.getElementById("play-dec");
+  var workInc = document.getElementById("work-inc");
+  var workDec = document.getElementById("work-dec");
+
+  playInc.addEventListener("click", incDec('inc', 'play'));
+
+  playDec.addEventListener("click", incDec('dec', 'play'));
+
+  workInc.addEventListener("click", incDec('inc', 'work'));
+
+  workDec.addEventListener("click", incDec('dec', 'work'));
+
   play = false;
-  initVal.innerHTML = '1:00';
-  defaultPlay = document.getElementById("play").innerHTML;
-  defaultWork = document.getElementById("work").innerHTML;
-  minutesToGo = parseInt(defaultWork);
-  secondsToGo = parseInt(defaultWork) * 60;
+
+  //initVal.innerHTML = '28:00';
+
   listen.addEventListener("click", function () {
-     interValID = window.setInterval(displayTime, 1000); 
+    defaultPlay = document.getElementById("play").innerHTML;
+    defaultWork = document.getElementById("work").innerHTML;
+    minutesToGo = parseInt(defaultWork);
+    secondsToGo = parseInt(defaultWork) * 60;
+    interValID = window.setInterval(displayTime, 1000); 
   });
 });
 
@@ -18,6 +34,7 @@ function displayTime()
   {
     if (play)
     {
+      activateAlarm();
       clearInterval(interValID);
     }
     activateAlarm();
@@ -39,6 +56,7 @@ function displayTime()
    
 }
 
+// will sound alarm for conclusion of work timer, then play timer
 function activateAlarm()
 {
   //clearInterval(interValID);
@@ -47,11 +65,60 @@ function activateAlarm()
   //TODO
 }
 
-function incrementDecrement()
+// closure for incrementing/decrementing play and work timer
+// so that it can be used as a callback with parameters(args)
+function incDec(dir, session)
 {
+  // retrieves existing value and increments/decrements accordingly
+  return function() {
+    var sessionType = document.getElementById(session);
+    var sessTypeVal = sessionType.innerHTML;
+    if (dir == 'inc')
+    {
+      sessTypeVal++;
+    }
+    else
+    {
+      sessTypeVal--;
+    }
 
+    // updates page with incremented/decremented timer value
+    sessionType.firstChild.nodeValue = sessTypeVal;
+
+    if (session == 'work')
+    {
+      var timerDisplay  = document.getElementById("timer"); 
+      timerDisplay.innerHTML = sessTypeVal + ':00';
+    }
+    // both if statements call css adjustment based on timer setting type
+    if (sessTypeVal > 9 && session == 'play')
+    {
+      dblDgtCss(session); 
+    }
+
+    if (sessTypeVal < 10 && session == 'work')
+    {
+      dblDgtCss(session);
+    }
+  }    
 }
 
+// adjusts css to prettify double digits to single digits
+// inside of black circle and single digits to double digits
+function dblDgtCss(sessType)
+{
+  var div = document.getElementById(sessType);
+  if (sessType == 'play')
+  {
+    div.style.left = "53px";
+  }
+  else if (sessType == 'work')
+  {
+    div.style.left = "76px";
+  }
+}
+
+// I think this can be deleted... deprecated
 function activateBreak()
 {
   //TODO
